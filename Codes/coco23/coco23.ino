21da4ace035f4aed8c2e637bfe50d05b0d3e510d
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2023-09-23 21:00:57 +0530

    edit12

--- Codes/coco23/coco23.ino
+++ Codes/coco23/coco23.ino
@@ -26,36 +26,37 @@ void setup() {
 
 
 void loop() {
-  if (!wallLeft()) {
-    leftAboutTurn();
-    delay(250);
-  }
-  while (wallFront()) {
-    rightAboutTurn();
-    delay(100);
-  }
+  
+//  if (!wallLeft()) {
+//    leftAboutTurn();
+//    delay(250);
+//  }
+//  while (wallFront()) {
+//    rightAboutTurn();
+//    delay(100);
+//  }
+//
+//  goCell();
+//  delay(250);
 
-  goCell();
-  delay(250);
+      while(true) {
+        if(!wallLeft()) {
+            leftAboutTurn();
+        }
 
-//      while(true) {
-////        if(!wallLeft()) {
-////            leftAboutTurn();
-////        }
-////
-////        else if(wallLeft() && wallRight() && wallFront()){
-////            turnBack();
-////  
-////          }
-////        else if (wallLeft()&& wallFront()){
-////          rightAboutTurn();
-////        }
-////             
-////        goCell();
-////            encoderPid();
-////            forwardBase();
-////            delay(50);
-//              rightAboutTurn();
-//              delay(1000);
-//        }
+        else if(wallLeft() && wallRight() && wallFront()){
+            turnBack();
+  
+          }
+        else if (wallLeft()&& wallFront()){
+          rightAboutTurn();
+        }
+             
+        goCell();
+            encoderPid();
+            forwardBase();
+            delay(50);
+              rightAboutTurn();
+              delay(1000);
+        }
 }
