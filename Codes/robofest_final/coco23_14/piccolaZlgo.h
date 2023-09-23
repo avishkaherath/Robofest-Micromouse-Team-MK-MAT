cddb4ee1a16aaa9e77e1604d8edb04a40ab62a9e
Melisa Oshaini Arukgoda <meleeshaarukgoda@gmail.com>
2023-09-23 23:27:07 +0530

    updated to_move funcs

--- Codes/robofest_final/coco23_14/piccolaZlgo.h
+++ Codes/robofest_final/coco23_14/piccolaZlgo.h
@@ -334,171 +334,171 @@ void floodFill3(){
 
 
 
-char toMove(byte x,byte y,byte xprev,byte yprev,byte orient){
+// char toMove(byte x,byte y,byte xprev,byte yprev,byte orient){
     
-    x_0,y_0,x_1,y_1,x_2,y_2,x_3,y_3 = getSurrounds(x,y);
+//     x_0,y_0,x_1,y_1,x_2,y_2,x_3,y_3 = getSurrounds(x,y);
     
-    byte val= flood[y][x];
-    byte prev=254;
-    byte minVals[4]={254,254,254,254};
-    byte visited[4]={2,2,2,2};
+//     byte val= flood[y][x];
+//     byte prev=254;
+//     byte minVals[4]={254,254,254,254};
+//     byte visited[4]={2,2,2,2};
 
 
 
-    if (isAccessible(x,y,x_0,y_0)){
-        if (x_0==xprev && y_0==yprev)
-            prev=0;
-        if (cells[y_0][x_0]!=0)
-            visited[0]= 1;
-        else
-            visited[0]= 2;
-        minVals[0]= flood[y_0][x_0];
-    }
-    else
-      minVals[0]=254;
+//     if (isAccessible(x,y,x_0,y_0)){
+//         if (x_0==xprev && y_0==yprev)
+//             prev=0;
+//         if (cells[y_0][x_0]!=0)
+//             visited[0]= 1;
+//         else
+//             visited[0]= 2;
+//         minVals[0]= flood[y_0][x_0];
+//     }
+//     else
+//       minVals[0]=254;
     
-    if (isAccessible(x,y,x_1,y_1)){
-        if (x_1==xprev && y_1==yprev)
-            prev=1;
-        if (cells[y_1][x_1]!=0)
-            visited[1]= 1;
-        else
-            visited[1]= 2;
+//     if (isAccessible(x,y,x_1,y_1)){
+//         if (x_1==xprev && y_1==yprev)
+//             prev=1;
+//         if (cells[y_1][x_1]!=0)
+//             visited[1]= 1;
+//         else
+//             visited[1]= 2;
             
-        minVals[1]= flood[y_1][x_1];
-    }
-    else
-      minVals[1]=254;
+//         minVals[1]= flood[y_1][x_1];
+//     }
+//     else
+//       minVals[1]=254;
       
-    if (isAccessible(x,y,x_2,y_2)){
-        if (x_2==xprev && y_2==yprev)
-            prev=2;
-        if (cells[y_2][x_2]!=0)
-            visited[2]= 1;
-        else
-            visited[2]= 2;
-        minVals[2]= flood[y_2][x_2];
-    }
-    else
-      minVals[2]=254;
+//     if (isAccessible(x,y,x_2,y_2)){
+//         if (x_2==xprev && y_2==yprev)
+//             prev=2;
+//         if (cells[y_2][x_2]!=0)
+//             visited[2]= 1;
+//         else
+//             visited[2]= 2;
+//         minVals[2]= flood[y_2][x_2];
+//     }
+//     else
+//       minVals[2]=254;
       
-    if (isAccessible(x,y,x_3,y_3)){
-        if (x_3==xprev && y_3==yprev)
-            prev=3;
-        if (cells[y_3][x_3]!=0)
-            visited[3]= 1;
-        else
-            visited[3]= 2;
-        minVals[3]= flood[y_3][x_3];
-    }
-    else
-      minVals[3]=254;
+//     if (isAccessible(x,y,x_3,y_3)){
+//         if (x_3==xprev && y_3==yprev)
+//             prev=3;
+//         if (cells[y_3][x_3]!=0)
+//             visited[3]= 1;
+//         else
+//             visited[3]= 2;
+//         minVals[3]= flood[y_3][x_3];
+//     }
+//     else
+//       minVals[3]=254;
 
     
-    byte minVal=254;
-    byte minCell=0;
-    byte noMovements=0;
+//     byte minVal=254;
+//     byte minCell=0;
+//     byte noMovements=0;
     
     
-    for (int i=0; i<4;i++){
-        if (minVals[i]!= 254){
-            noMovements+=1;}
-    }
+//     for (int i=0; i<4;i++){
+//         if (minVals[i]!= 254){
+//             noMovements+=1;}
+//     }
     
-    for (int i=0; i<4;i++){
-        if (minVals[i]<minVal){
-            if (noMovements==1){
-                minVal= minVals[i];
-                minCell= i;
-            }
-            else{
-                if(i!=prev){
-                    minVal= minVals[i];
-                    minCell= i;
+//     for (int i=0; i<4;i++){
+//         if (minVals[i]<minVal){
+//             if (noMovements==1){
+//                 minVal= minVals[i];
+//                 minCell= i;
+//             }
+//             else{
+//                 if(i!=prev){
+//                     minVal= minVals[i];
+//                     minCell= i;
                     
-                }
-            }
-        }
-    }
+//                 }
+//             }
+//         }
+//     }
 
 
-    byte bla[4] ={254,254,254,254};
-    for (int i=0; i<4;i++){
-        if (minVals[i]==minVal)
-            bla[i]= visited[i];
-    }
+//     byte bla[4] ={254,254,254,254};
+//     for (int i=0; i<4;i++){
+//         if (minVals[i]==minVal)
+//             bla[i]= visited[i];
+//     }
 
-    byte blaCount=0;
-    for (int i=0; i<4;i++){
-        if (bla[i]== 2){
-            minCell=i ;
-            blaCount+=1;
-        }
-    }
+//     byte blaCount=0;
+//     for (int i=0; i<4;i++){
+//         if (bla[i]== 2){
+//             minCell=i ;
+//             blaCount+=1;
+//         }
+//     }
       
-    if (blaCount==0){
-        for (int i=0; i<4;i++){
-            if (bla[i]== 1)
-                minCell=i;
-        }
-    }
+//     if (blaCount==0){
+//         for (int i=0; i<4;i++){
+//             if (bla[i]== 1)
+//                 minCell=i;
+//         }
+//     }
 
 
 
     
-    if (minCell==orient)
-        return ('F');
-    else if((minCell==orient-1) || (minCell== orient+3))
-        return('L');
-    else if ((minCell==orient+1) || (minCell== orient-3))
-        return('R');
-    else
-        return('B');
+//     if (minCell==orient)
+//         return ('F');
+//     else if((minCell==orient-1) || (minCell== orient+3))
+//         return('L');
+//     else if ((minCell==orient+1) || (minCell== orient-3))
+//         return('R');
+//     else
+//         return('B');
 
 
-}
+// }
 
 
-char toMove2(){
+// char toMove2(){
 
-    x_0,y_0,x_1,y_1,x_2,y_2,x_3,y_3 = getSurrounds(x,y);
+//     x_0,y_0,x_1,y_1,x_2,y_2,x_3,y_3 = getSurrounds(x,y);
     
-    byte val= flood2[y][x];
-    byte minCell=254;
-
-    
-    if (isAccessible(x,y,x_0,y_0)){
-        if (flood2[y_0][x_0]==val-1)
-            minCell=0;
-    }
-
-    if (isAccessible(x,y,x_1,y_1)){
-        if (flood2[y_1][x_1]==val-1)
-            minCell=1;
-    }
-
-    if (isAccessible(x,y,x_2,y_2)){
-        if (flood2[y_2][x_2]==val-1)
-            minCell=2;
-    }
-
-    if (isAccessible(x,y,x_3,y_3)){
-        if (flood2[y_3][x_3]==val-1)
-            minCell=3;
-    }
-    
+//     byte val= flood2[y][x];
+//     byte minCell=254;
 
     
-    if (minCell==orient)
-        dir= 'F';
-    else if((minCell==orient-1) || (minCell== orient+3))
-        dir= 'L';
-    else if ((minCell==orient+1) || (minCell== orient-3))
-        dir= 'R';
-    else
-        dir= 'B';
+//     if (isAccessible(x,y,x_0,y_0)){
+//         if (flood2[y_0][x_0]==val-1)
+//             minCell=0;
+//     }
 
-}
+//     if (isAccessible(x,y,x_1,y_1)){
+//         if (flood2[y_1][x_1]==val-1)
+//             minCell=1;
+//     }
+
+//     if (isAccessible(x,y,x_2,y_2)){
+//         if (flood2[y_2][x_2]==val-1)
+//             minCell=2;
+//     }
+
+//     if (isAccessible(x,y,x_3,y_3)){
+//         if (flood2[y_3][x_3]==val-1)
+//             minCell=3;
+//     }
+    
+
+    
+//     if (minCell==orient)
+//         dir= 'F';
+//     else if((minCell==orient-1) || (minCell== orient+3))
+//         dir= 'L';
+//     else if ((minCell==orient+1) || (minCell== orient-3))
+//         dir= 'R';
+//     else
+//         dir= 'B';
+
+// }
 
 
 
